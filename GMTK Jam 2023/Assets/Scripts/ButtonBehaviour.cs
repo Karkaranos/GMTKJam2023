6c14423909075d19d04c8a3a13967b0b4a6862d9
Karkaranos <karkaranosyt@gmail.com>
2023-07-08 19:26:00 -0500

    Scene FIx

--- GMTK Jam 2023/Assets/Scripts/ButtonBehaviour.cs
+++ GMTK Jam 2023/Assets/Scripts/ButtonBehaviour.cs
@@ -10,7 +10,7 @@ public class ButtonBehaviour : MonoBehaviour
 
     public void PlayButton()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void ControlsButton()
